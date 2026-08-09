595840f1e9f84e85649a5abaf4c0125093e4cca9
zaimcherosli <huzaimrosli@gmail.com>
2026-08-10 00:59:18 +0800

    Deploy dist folder setup for Capacitor Android build v1.1.9

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
    PWA SERVICE WORKER - CACHING & OFFLINE ENGINE
    ========================================================================== */
 
-const CACHE_NAME = 'waktu-solat-v1.1.8';
+const CACHE_NAME = 'waktu-solat-v1.1.9';
 const STATIC_ASSETS = [
   './',
   './index.html',
