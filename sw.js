7f506c754d31fa693c1933d96e1fe52d4183fdf7
zaimcherosli <huzaimrosli@gmail.com>
2026-08-10 11:15:35 +0800

    Soften quote card background gradient for clear text contrast v1.3.3

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
    PWA SERVICE WORKER - CACHING & OFFLINE ENGINE
    ========================================================================== */
 
-const CACHE_NAME = 'waktu-solat-v1.3.2';
+const CACHE_NAME = 'waktu-solat-v1.3.3';
 const STATIC_ASSETS = [
   './',
   './index.html',
