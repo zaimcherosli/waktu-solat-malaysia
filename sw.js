a56f70d943f34b148a06479179860ae889bae611
zaimcherosli <huzaimrosli@gmail.com>
2026-08-10 14:18:28 +0800

    Fix unclosed HTML tags in tab-tasbih causing layout shift and hiding tab-quran v2.0.1

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
    PWA SERVICE WORKER - CACHING & OFFLINE ENGINE
    ========================================================================== */
 
-const CACHE_NAME = 'waktu-solat-v2.0.0';
+const CACHE_NAME = 'waktu-solat-v2.0.1';
 const STATIC_ASSETS = [
   './',
   './index.html',
