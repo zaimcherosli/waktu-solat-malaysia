dae00d2c26904860df3a83e78a0e79a9d908a6e8
zaimcherosli <huzaimrosli@gmail.com>
2026-08-10 12:34:38 +0800

    Implement 100% bulletproof inline PWA install guide modal popup and button handlers v1.4.5

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
    PWA SERVICE WORKER - CACHING & OFFLINE ENGINE
    ========================================================================== */
 
-const CACHE_NAME = 'waktu-solat-v1.4.1';
+const CACHE_NAME = 'waktu-solat-v1.4.5';
 const STATIC_ASSETS = [
   './',
   './index.html',
