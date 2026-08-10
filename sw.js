971604787929a62c7cbf2a27d403699728207c42
zaimcherosli <huzaimrosli@gmail.com>
2026-08-10 11:41:08 +0800

    Adjust bottom container padding and footer margins for uniform spacing above and below credit text v1.3.7

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
    PWA SERVICE WORKER - CACHING & OFFLINE ENGINE
    ========================================================================== */
 
-const CACHE_NAME = 'waktu-solat-v1.3.6';
+const CACHE_NAME = 'waktu-solat-v1.3.7';
 const STATIC_ASSETS = [
   './',
   './index.html',
